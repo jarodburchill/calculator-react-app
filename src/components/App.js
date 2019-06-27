67bdcdd4296e6c1ca101bb4045a98c8b25c3944d
BURCHILL <jarodburchill@gmail.com>
2019-06-27 11:31:37 -0400

    Added buttons

--- src/components/App.js
+++ src/components/App.js
@@ -1,9 +1,33 @@
 import React from 'react';
+import Button from "./Button/Button";
 
 const App = () => {
+  const data = require('../buttons.json');
+  const buttons = data.buttons;
+
+  const renderNewLine = (newLine) => {
+    if (newLine) {
+      return (
+        <br />
+      );
+    }
+  }
+
   return (
-    <div>
-    </div>
+    <>
+      <input />
+      <br />
+      {buttons.map((item, index) => {
+        return (
+          <span key={index}>
+            <Button
+              symbol={item.symbol}
+            />
+            {renderNewLine(item.newLine)}
+          </span>
+        );
+      })}
+    </>
   );
 }
 
---
+++ src/components/Button/Button.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Button = (props) => {
+  return (
+    <button>
+      {props.symbol}
+    </button>
+  );
+}
+
+export default Button;
